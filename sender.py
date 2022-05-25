bee3f6926a676592df82b5dac0d55ab2e31aab5b
therealphantom <therealphantom@mail.ru>
2022-05-25 23:48:40 +0300

    Update to v1.0.1

--- sender.py
+++ sender.py
@@ -17,6 +17,29 @@ bot = commands.Bot(command_prefix = 's!')
 @bot.event
 async def on_ready(): # отправка сообщения в канал, который находится по айди
 	while True:
-		await bot.get_channel(int(channel_id)).send(input(': '))
+		message = input(': ')
+
+		if(message == 's!embed'): # эмбед
+			title = input('Введите заголовок: ')
+			description = input('Введите описание: ')
+			footer = input('Введите футер: ')
+
+			embed = discord.Embed(title = title, description = description).set_footer(text = footer)
+
+			await bot.get_channel(int(channel_id)).send(embed = embed)
+			continue
+
+		if(message == 's!file'): # отправить файл
+			filename = input('Введите путь к файлу: ')
+
+			await bot.get_channel(int(channel_id)).send(file = discord.File(filename))
+			continue
+
+		if(message == 's!help'):
+			print('\nСписок команд:\n')
+			print('s!embed - Отправить Embed\ns!file - Отправить файл\ns!help - Выводит это сообщение')
+			continue
+
+		await bot.get_channel(int(channel_id)).send(message)
 
 bot.run(token)
